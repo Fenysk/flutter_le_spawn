b725f785c5319067ced65aeac43306edcff070df
Fenysk <minefenysk@gmail.com>
2024-06-11 09:25:33 +0200

    💄 Game cards and tiles

--- lib/ui/widgets/game/video_game_item.dart
+++ lib/ui/widgets/game/video_game_item.dart
@@ -15,10 +15,14 @@ class VideoGameItem extends StatelessWidget {
       case 'list':
         mainContent = ListTile(
           contentPadding: const EdgeInsets.symmetric(horizontal: 8),
-          leading: FadeInImage.assetNetwork(
-            placeholder: 'assets/images/placeholder.webp',
-            image: videoGame.VideoGame.images[0],
-            imageErrorBuilder: (context, error, stackTrace) => Image.asset('assets/images/placeholder.webp'),
+          leading: AspectRatio(
+            aspectRatio: 4 / 3,
+            child: FadeInImage.assetNetwork(
+              placeholder: 'assets/images/placeholder.webp',
+              image: videoGame.VideoGame.images[0],
+              imageErrorBuilder: (context, error, stackTrace) => Image.asset('assets/images/placeholder.webp'),
+              fit: BoxFit.cover,
+            ),
           ),
           title: Text(videoGame.VideoGame.title, overflow: TextOverflow.ellipsis),
           subtitle: Text('${videoGame.edition} · ${videoGame.region} · ${videoGame.VideoGame.Platform.name}'),
@@ -35,23 +39,30 @@ class VideoGameItem extends StatelessWidget {
                 placeholder: 'assets/images/placeholder.webp',
                 image: videoGame.VideoGame.images[0],
                 imageErrorBuilder: (context, error, stackTrace) => Image.asset('assets/images/placeholder.webp'),
+                fit: BoxFit.cover,
+                width: double.infinity,
               ),
               Positioned.fill(
                 child: DecoratedBox(
                   decoration: BoxDecoration(
                     gradient: LinearGradient(
-                      colors: [
-                        Colors.black.withOpacity(0.8),
-                        Colors.transparent
-                      ],
-                      begin: Alignment.bottomCenter,
-                      end: Alignment.topCenter,
-                    ),
+                        colors: [
+                          Colors.black.withOpacity(0.9),
+                          Colors.black.withOpacity(0.5),
+                          Colors.transparent
+                        ],
+                        begin: Alignment.bottomCenter,
+                        end: Alignment.topCenter,
+                        stops: const [
+                          0,
+                          0.3,
+                          1
+                        ]),
                   ),
                 ),
               ),
               Positioned(
-                bottom: 10,
+                bottom: 6,
                 left: 10,
                 right: 10,
                 child: Container(
